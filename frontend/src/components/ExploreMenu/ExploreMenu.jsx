7dd10f97524a1926eb1658a7345e17fab2636c31
luciano <lazaluciano327@gmail.com>
2025-10-10 16:34:36 +0300

    Provider

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -17,7 +17,7 @@ const ExploreMenu = ({ category, setCategory }) => {
         {menu_list.map((item, index) => {
           return (
             <div
-              onMouseEnter={() =>
+              onClick={() =>
                 setCategory((prev) =>
                   prev === item.menu_name ? "All" : item.menu_name
                 )
